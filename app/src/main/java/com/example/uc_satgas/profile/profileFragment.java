ba93e26bb7e625024ae807920493252844c3ddd3
pjulianyehuda <pjulianyehuda@student.ciputra.id>
2021-01-22 18:15:28 +0800

    new

---
+++ app/src/main/java/com/example/uc_satgas/profile/profileFragment.java
@@ -0,0 +1,47 @@
+package com.example.uc_satgas.profile;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.uc_satgas.R;
+
+
+public class profileFragment extends Fragment {
+
+
+
+    public profileFragment() {
+        // Required empty public constructor
+    }
+    TextView account_nama, account_email;
+    Button button_logout;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+
+        account_nama = view.findViewById(R.id.account_nama);
+        account_email = view.findViewById(R.id.account_email);
+
+        button_logout = view.findViewById(R.id.button_logout);
+
+        button_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/uc_satgas/splash/splashFragment.java
@@ -0,0 +1,51 @@
+package com.example.uc_satgas.splash;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import com.example.uc_satgas.R;
+
+public class splashFragment extends Fragment {
+
+    public  splashFragment(){
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        new Handler().postDelayed(() -> {
+            NavDirections action = SplashFragmentDirections.actionMain();
+            Navigation.findNavController(view).navigate(action);
+        }, 2000);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+    }
+
+}
